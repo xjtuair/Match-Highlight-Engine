a463ea3c3e0644cc5ce8f550e2815cecf23292a9
xjtuair <184145933+xjtuair@users.noreply.github.com>
2023-05-09 18:32:56 +0000

    maintainer-mode changes

--- generator.py
+++ generator.py
@@ -5,3 +5,9 @@ import numpy as np
 import pandas as pd
 import IPython.display as ipd 
 import os,shutil
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+
+#Load the audio file of the sports clip.
+filename='M27 KKR vs RCB  – Match Highlights.mp3' #Enter your audio file name of match here. .wav,.mp3, etc. are supported.
