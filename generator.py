aa139a515b1365911a46dfd02fdb666b177cc9b4
xjtuair <184145933+xjtuair@users.noreply.github.com>
2023-05-16 05:29:13 +0000

    updates dependencies

--- generator.py
+++ generator.py
@@ -71,3 +71,9 @@ end=np.array(df['end'])
 #Create temporary folder for storing subclips generated. This folder will be deleted later after highlights are generated. 
 cwd=os.getcwd()
 sub_folder=os.path.join(cwd,"Subclips")
+if os.path.exists(sub_folder):
+	shutil.rmtree(sub_folder)
+	path=os.mkdir(sub_folder)
+else:
+	path=os.mkdir(sub_folder)
+#print(sub_folder,type(sub_folder))
